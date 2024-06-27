3b26bbf0c1d036c61f389c9bdb6a89fa718b5563
Chenchen Zhu <zhuchenchen1001@qq.com>
2024-06-27 20:36:24 +0800

    add benchmark results to spinlock

---
+++ go.mod
@@ -0,0 +1,3 @@
+module GoPool
+
+go 1.22.2
--- internal/spinlock_test.go
+++ internal/spinlock_test.go
@@ -5,6 +5,17 @@ import (
 	"testing"
 )
 
+/**
+benchmark results
+goos: windows
+goarch: amd64
+pkg: GoPool/internal
+cpu: 13th Gen Intel(R) Core(TM) i5-13600K
+BenchmarkMutex-20       	    20655360                56.97 ns/op
+BenchmarkSpinLockOrigin-20      86752840                12.83 ns/op
+BenchmarkSpinLockBackoff-20     95682086                12.65 ns/op
+*/
+
 func BenchmarkMutex(b *testing.B) {
 	m := sync.Mutex{}
 	b.RunParallel(func(pb *testing.PB) {

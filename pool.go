fc6802f9aa7ad6f400ed6caf7f5a941bae2fb16d
Chenchen Zhu <zhuchenchen1001@qq.com>
2024-06-27 20:45:43 +0800

    add pool.go

---
+++ pool.go
@@ -0,0 +1,20 @@
+package GoPool
+
+import (
+	"context"
+	"sync"
+)
+
+type Pool struct {
+	capacity      int32
+	running       int32
+	lock          sync.Locker
+	workers       workerArray
+	state         int32
+	cond          *sync.Cond
+	workerCache   sync.Pool
+	waiting       int32
+	heartbeatDone int32
+	stopHeartBeat context.CancelFunc
+	options       *Options
+}

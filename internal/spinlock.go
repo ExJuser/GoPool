16907e042a7c585ae30873aeee13548380681b24
Chenchen Zhu <zhuchenchen1001@qq.com>
2024-06-27 19:43:00 +0800

    add comments to spinLock

--- internal/spinlock.go
+++ internal/spinlock.go
@@ -6,16 +6,19 @@ import (
 	"sync/atomic"
 )
 
-type spinLock uint32
+const maxBackoff = 16 // 最大退避次数
 
-const maxBackoff = 16
+type spinLock uint32
 
 func (l *spinLock) Lock() {
 	backoff := 1
+	// 尝试使用原子操作将锁从0设置为1，表示加锁成功
 	for !atomic.CompareAndSwapUint32((*uint32)(l), 0, 1) {
+		// 如果加锁失败，进行退避等待
 		for i := 0; i < backoff; i++ {
-			runtime.Gosched()
+			runtime.Gosched() // 让出 CPU 时间片，避免忙等
 		}
+		//不超过最大退避次数
 		if backoff < maxBackoff {
 			backoff <<= 1
 		}
@@ -23,7 +26,7 @@ func (l *spinLock) Lock() {
 }
 
 func (l *spinLock) Unlock() {
-	atomic.StoreUint32((*uint32)(l), 0)
+	atomic.StoreUint32((*uint32)(l), 0) //解锁只需原子操作将锁置为0即可
 }
 
 func NewSpinLock() sync.Locker {

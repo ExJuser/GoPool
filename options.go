39d0d09d5fff1a57cb054e15ec8e637ee9c49ae1
Chenchen Zhu <zhuchenchen1001@qq.com>
2024-06-28 18:51:13 +0800

    edit options.go:add some comments

--- options.go
+++ options.go
@@ -14,13 +14,18 @@ func loadOptions(options ...Option) *Options {
 	return opts
 }
 
+// Options 一些实例化协程池时的可选参数
 type Options struct {
-	ExpiryDuration   time.Duration
-	PreAlloc         bool
+	//距离上一次使用间隔超过 ExpiryDuration 的worker被认为是过期
+	ExpiryDuration time.Duration
+	//标志在初始化协程池时是否提前分配内存
+	PreAlloc bool
+	//因为向协程池提交任务被阻塞的最大协程数量
 	MaxBlockingTasks int
-	NonBlocking      bool
-	PanicHandler     func(interface{})
-	Logger           Logger
+	//非阻塞式 不会因为向协程池提交任务被阻塞 直接返回错误
+	NonBlocking  bool
+	PanicHandler func(interface{})
+	Logger       Logger
 }
 
 func WithOptions(options Options) Option {

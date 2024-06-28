f167200acf6c3432974769f850a614da53c0b480
Chenchen Zhu <zhuchenchen1001@qq.com>
2024-06-28 12:44:27 +0800

    add some comments to worker_stack.go

--- worker_stack.go
+++ worker_stack.go
@@ -33,6 +33,7 @@ func (w *workerStack) detach() *goWorker {
 	if l == 0 {
 		return nil
 	}
+	//栈 后进先出
 	worker := w.items[l-1]
 	w.items[l-1] = nil
 	w.items = w.items[:l-1]
@@ -44,15 +45,20 @@ func (w *workerStack) retrieveExpiry(duration time.Duration) []*goWorker {
 	if n == 0 {
 		return nil
 	}
+	//已经有 duration 没有被使用过的worker认为是已经过期
+	//即recycleTime早于expiryTime的worker被认为是过期
 	expiryTime := time.Now().Add(-duration)
 	w.expiry = w.expiry[:0]
+	//由于先加入栈的一定更早过期 采用二分查找快速找到过期的worker
 	if index := w.binarySearch(0, n-1, expiryTime); index != -1 {
 		w.expiry = append(w.expiry, w.items[:index+1]...)
+		//将没有过期的worker填充到items的前面
 		m := copy(w.items, w.items[index+1:])
+		//清空已经过期的worker的内存防止内存泄漏
 		for i := m; i < n; i++ {
 			w.items[i] = nil
 		}
-		w.items = w.items[m:]
+		w.items = w.items[:m]
 	}
 	return w.expiry
 }

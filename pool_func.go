ad3f117e97ad5f9ca0ea601f57ca198176a61ae8
Chenchen Zhu <zhuchenchen1001@qq.com>
2024-06-29 21:14:39 +0800

    edit pool_func.go and worker_func.go

--- pool_func.go
+++ pool_func.go
@@ -1,1 +1,131 @@
 package GoPool
+
+import (
+	"GoPool/internal"
+	"context"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+type PoolWithFunc struct {
+	capacity      int32
+	running       int32
+	lock          sync.Locker
+	workers       []*goWorkerWithFunc
+	state         int32
+	cond          *sync.Cond
+	poolFunc      func(interface{})
+	workerCache   sync.Pool
+	waiting       int32
+	heartbeatDone int32
+	stopHeartbeat context.CancelFunc
+	options       *Options
+}
+
+func (p *PoolWithFunc) purgePeriodically(ctx context.Context) {
+	heartbeat := time.NewTicker(p.options.ExpiryDuration)
+	defer func() {
+		heartbeat.Stop()
+		atomic.StoreInt32(&p.heartbeatDone, 1)
+	}()
+
+	var expiredWorkers []*goWorkerWithFunc
+	for {
+		select {
+		case <-heartbeat.C:
+		case <-ctx.Done():
+			return
+		}
+		if p.IsClosed() {
+			break
+		}
+		currentTime := time.Now()
+		p.lock.Lock()
+		idleWorkers := p.workers
+		n := len(idleWorkers)
+		var i int
+		for i = 0; i < n && currentTime.Sub(idleWorkers[i].recycleTime) > p.options.ExpiryDuration; i++ {
+		}
+		expiredWorkers = append(expiredWorkers[:0], idleWorkers[:i]...)
+		if i > 0 {
+			m := copy(idleWorkers, idleWorkers[i:])
+			for i = m; i < n; i++ {
+				idleWorkers[i] = nil
+			}
+			p.workers = idleWorkers[:m]
+		}
+		p.lock.Unlock()
+		for i, w := range expiredWorkers {
+			w.args <- nil
+			expiredWorkers[i] = nil
+		}
+		if p.Running() == 0 || (p.Waiting() > 0 && p.Free() > 0) {
+			p.cond.Broadcast()
+		}
+	}
+}
+
+func NewPoolWithFunc(size int, pf func(interface{}), options ...Option) (*PoolWithFunc, error) {
+	if size <= 0 {
+		size = -1
+	}
+	if pf == nil {
+		return nil, ErrLackPoolFunc
+	}
+	opts := loadOptions(options...)
+	if expiry := opts.ExpiryDuration; expiry < 0 {
+		return nil, ErrInvalidPoolExpiry
+	} else if expiry == 0 {
+		opts.ExpiryDuration = DefaultCleanIntervalTime
+	}
+
+	if opts.Logger == nil {
+		opts.Logger = defaultLogger
+	}
+	p := &PoolWithFunc{
+		capacity: int32(size),
+		poolFunc: pf,
+		lock:     internal.NewSpinLockBackoff(),
+		options:  opts,
+	}
+	p.workerCache.New = func() any {
+		return &goWorkerWithFunc{
+			pool: p,
+			args: make(chan interface{}, workerChanCap()),
+		}
+	}
+	if p.options.PreAlloc {
+		if size == -1 {
+			return nil, ErrInvalidPreAllocSize
+		}
+		p.workers = make([]*goWorkerWithFunc, 0, size)
+	}
+	p.cond = sync.NewCond(p.lock)
+
+	var ctx context.Context
+	ctx, p.stopHeartbeat = context.WithCancel(context.Background())
+	go p.purgePeriodically(ctx)
+	return p, nil
+}
+
+func (p *PoolWithFunc) Running() int {
+	return int(atomic.LoadInt32(&p.running))
+}
+func (p *PoolWithFunc) Waiting() int {
+	return int(atomic.LoadInt32(&p.waiting))
+}
+func (p *PoolWithFunc) Cap() int {
+	return int(atomic.LoadInt32(&p.capacity))
+}
+func (p *PoolWithFunc) Free() int {
+	var c int
+	if c = p.Cap(); c < 0 {
+		return -1
+	}
+	return c - p.Running()
+}
+
+func (p *PoolWithFunc) IsClosed() bool {
+	return atomic.LoadInt32(&p.state) == CLOSED
+}
--- worker_func.go
+++ worker_func.go
@@ -1,1 +1,13 @@
 package GoPool
+
+import "time"
+
+type goWorkerWithFunc struct {
+	pool        *PoolWithFunc
+	args        chan interface{}
+	recycleTime time.Time
+}
+
+func (w *goWorkerWithFunc) run() {
+
+}

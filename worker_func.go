2807199951feccc905138c756e52fef7d50f5a6f
Chenchen Zhu <zhuchenchen1001@qq.com>
2024-06-29 21:37:40 +0800

    edit worker_func.go:add func run

--- worker_func.go
+++ worker_func.go
@@ -1,6 +1,9 @@
 package GoPool
 
-import "time"
+import (
+	"runtime"
+	"time"
+)
 
 type goWorkerWithFunc struct {
 	pool        *PoolWithFunc
@@ -9,5 +12,32 @@ type goWorkerWithFunc struct {
 }
 
 func (w *goWorkerWithFunc) run() {
+	w.pool.addRunning(1)
+	go func() {
+		defer func() {
+			w.pool.addRunning(-1)
+			w.pool.workerCache.Put(w)
+			if p := recover(); p != nil {
+				if ph := w.pool.options.PanicHandler; ph != nil {
+					ph(p)
+				} else {
+					w.pool.options.Logger.Printf("worker with func exits from a panic: %v\n", p)
+					var buf [4096]byte
+					n := runtime.Stack(buf[:], false)
+					w.pool.options.Logger.Printf("worker with func exits from panic: %s\n", string(buf[:n]))
+				}
+			}
+			w.pool.cond.Signal()
+		}()
 
+		for args := range w.args {
+			if args == nil {
+				return
+			}
+			w.pool.poolFunc(args)
+			if ok := w.pool.revertWorker(w); !ok {
+				return
+			}
+		}
+	}()
 }

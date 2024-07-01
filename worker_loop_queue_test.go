7bc38e31e62f8db045d4353a4a2685ca10731018
Chenchen Zhu <zhuchenchen1001@qq.com>
2024-07-01 14:33:31 +0800

    edit worker_loop_queue.test:test func len(),isEmpty(),detach()

--- worker_loop_queue_test.go
+++ worker_loop_queue_test.go
@@ -1,1 +1,14 @@
 package GoPool
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewLoopQueue(t *testing.T) {
+	size := 100
+	q := newWorkerLoopQueue(size)
+	assert.EqualValues(t, 0, q.len(), "Len error")
+	assert.Equal(t, true, q.isEmpty(), "IsEmpty error")
+	assert.Nil(t, q.detach(), "Detach error")
+}
